4930571d85db3708b74dd72bccc31db9a354dc84
= <Luis.PARODY@naranja.com>
2019-09-01 00:20:37 -0300

    Version bajada del curso

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const app = express();
+
 const hbs = require('hbs');
 require('./hbs/helpers');
 
-//const port = process.env.PORT || 3000;
-app.set('port',process.env.PORT || 3000)
+const port = process.env.PORT || 3000;
 
-app.use( express.static( __dirname+ '/public') ) ;
 
-// HBS
-hbs.registerPartials(__dirname+'/views/parciales');
-app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/',  (req, res) => {
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
 
-    res.render('home',
-        {
-            nombre: 'luIs pArody'
-        });
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'fernando'
+    });
 });
 
-app.get('/about',(req, res) => {
+app.get('/about', (req, res) => {
+
     res.render('about');
-})
+});
 
-
-app.listen(app.get('port'),() => {
-    console.log('Server en puerto: ',app.get('port'));
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
---
+++ views/readme.md
@@ -0,0 +1,2 @@
+"
+reeadme
